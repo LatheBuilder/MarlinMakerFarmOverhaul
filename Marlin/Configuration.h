cf69fc760166f6fd7101d96d619bf95ad97966a8
Jason Burton <lathebuilder@gmail.com>
2020-05-27 00:22:26 -0500

    Final probe Z offset? -0.93

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -965,7 +965,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, -48.6, -0.90 } //jason
+#define NOZZLE_TO_PROBE_OFFSET { 0, -48.6, -0.93 } //jason
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
